6f09dab0b731c31d640f9f8452669427c9475f99
hecdelatorre <hector982015@gmail.com>
2023-02-10 12:11:38 -0600

    10-02-2023 12:11:38

--- index.sh
+++ index.sh
@@ -1,78 +1,96 @@
 #!/bin/bash
 
-# Ask the user how many downloads they want to perform
-read -p "How many downloads do you want to perform? " count
+# Function to ask the user for the number of downloads
+get_download_count() {
+  read -p "How many downloads do you want to perform? " count
+}
 
-# Initialize an empty array to store the links
-links=()
-
-# Ask the user for the download directory
-while true; do
-  read -p "Enter the download directory: " directory
-
-  # Check if the directory is a valid directory
-  if [ -d "$directory" ]; then
-    break
-  fi
-
-  echo "Invalid directory. Please enter a valid directory."
-done
-
-# Ask the user if they want to create a new folder
-while true; do
-  read -p "Do you want to create a new folder in the download directory? [y/n] " answer
-
-  if [ "$answer" == "y" ] || [ "$answer" == "n" ]; then
-    break
-  fi
-
-  echo "Invalid input. Please enter either 'y' or 'n'."
-done
-
-# If the user wants to create a new folder, ask for the folder name and validate if it doesn't already exist
-if [ "$answer" == "y" ]; then
+# Function to ask the user for the download directory and validate it
+get_download_directory() {
   while true; do
-    read -p "Enter the name of the new folder: " folder
+    read -p "Enter the download directory: " directory
 
-    # Check if the folder doesn't already exist
-    if [ ! -d "$directory/$folder" ]; then
-      mkdir "$directory/$folder"
-      directory="$directory/$folder"
+    if [ -d "$directory" ]; then
       break
     fi
 
-    echo "A folder with that name already exists. Please enter a different name."
+    echo "Invalid directory. Please enter a valid directory."
   done
-fi
+}
 
-# Ask the user to input the links
-for ((i=0; i<count; i++)); do
+# Function to ask the user if they want to create a new folder and get the folder name
+get_new_folder() {
   while true; do
-    read -p "Enter link $((i + 1)): " link
+    read -p "Do you want to create a new folder in the download directory? [y/n] " answer
 
-    # Check if the link is a valid HTTP, HTTPS, or magnet link
-    if [[ "$link" =~ ^https?://.* ]] || [[ "$link" =~ xt=urn:btih:[a-zA-Z0-9]+ ]]; then
-      links+=( "$link" )
+    if [ "$answer" == "y" ] || [ "$answer" == "n" ]; then
       break
     fi
 
-    echo "Invalid link. Please enter a valid HTTP, HTTPS, or magnet link."
+    echo "Invalid input. Please enter either 'y' or 'n'."
   done
-done
 
-# Download the files using aria2c and record the start time and end time of each download
-start_time=$(date +%s)
+  if [ "$answer" == "y" ]; then
+    while true; do
+      read -p "Enter the name of the new folder: " folder
 
-for ((i=0; i<count; i++)); do
-  link="${links[$i]}"
-  echo "Downloading file $((i + 1)) of $count..."
-  aria2c "$link" -d "$directory" --seed-time=0
-done
+      if [ ! -d "$directory/$folder" ]; then
+        mkdir "$directory/$folder"
+        directory="$directory/$folder"
+        break
+      fi
 
-end_time=$(date +%s)
+      echo "A folder with that name already exists. Please enter a different name."
+    done
+  fi
+}
 
-# Show a summary of the downloaded files along with the download time
-total_time=$((end_time - start_time))
-echo "Downloads complete."
-echo "Downloaded files are stored in $directory."
-echo "Download time: $total_time seconds."
+# Function to ask the user for the links and validate them
+get_links() {
+  for ((i=0; i<count; i++)); do
+    while true; do
+      read -p "Enter link $((i + 1)): " link
+
+      if [[ "$link" =~ ^https?://.* ]] || [[ "$link" =~ xt=urn:btih:[a-zA-Z0-9]+ ]]; then
+        links+=( "$link" )
+        break
+      fi
+
+      echo "Invalid link. Please enter a valid HTTP, HTTPS, or magnet link."
+    done
+  done
+}
+
+# Function to download the files using aria2c and record the download time
+download_files() {
+  start_time=$(date +%s)
+
+  for ((i=0; i<count; i++)); do
+    link="${links[$i]}"
+    echo -e "\nDownloading file $((i + 1)) of $count...\n"
+    aria2c "$link" -d "$directory" --seed-time=0
+  done
+
+  end_time=$(date +%s)
+  total_time=$((end_time - start_time))
+}
+
+# Function to display a summary of the downloaded files and the download time
+show_summary() {
+  echo "Downloads complete."
+  echo "Downloaded files are stored in $directory."
+  echo "Download time: $total_time seconds."
+}
+
+# Main function to call all the functions
+main() {
+  get_download_count
+  get_download_directory
+  get_new_folder
+  get_links
+  download_files
+  show_summary
+}
+
+# Call the main function
+main
